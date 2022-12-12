3bb490537e6dd06cc33610ce76ea0aa4422673a6
DrYoe <100612710+DrYoe@users.noreply.github.com>
2022-12-12 15:39:22 +0630

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5244640547:AAFF0M3FEZj78pyHdFwzqyQf8LU40TpGcE8")
       API_ID = int(os.environ.get("APP_ID", 12158462))
       API_HASH = os.environ.get("API_HASH", "0b962717d931f4480c46d56c85d409a5")
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "bottom")
